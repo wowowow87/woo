de5a843920389c1ba84518b65f1a0a1e91400c2a
wowow87 <a24.n83b@g.chuo-u.ac.jp>
2024-11-19 17:25:45 +0900

    Initial Commit

---
+++ woo/wooApp.swift
@@ -0,0 +1,17 @@
+//
+//  wooApp.swift
+//  woo
+//
+//  Created by 金山　花南  on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct wooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
